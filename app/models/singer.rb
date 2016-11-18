aaa068f6c386c89f7a97c1c4c24da108bcdd060b
aaaa1379 <aaaa1379@gmail.com>
2016-11-18 15:21:18 +0800

    add translation view

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,3 +1,11 @@
 class Singer < ApplicationRecord
   has_many :songs
+
+  def name_info
+    if self.name_translation.blank?
+      "#{self.name}"
+    else
+      "#{self.name} / #{singer.name_translation}"
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,12 @@ class Song < ApplicationRecord
 
   belongs_to :singer  , :class_name => "Singer", :foreign_key => "singer_id"
   belongs_to :composer, :class_name => "Singer", :foreign_key => "composer_id"
+
+  def title_info
+    if self.title_translation.blank?
+      "#{self.title}"
+    else
+      "#{self.title} / #{self.title_translation}"
+    end
+  end
 end
