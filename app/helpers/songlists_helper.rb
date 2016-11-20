c6a9b195aa20fab2a7a9808ae897a7cc727d3969
aaaa1379 <aaaa1379@gmail.com>
2016-11-20 20:52:17 +0800

    remove sonlist helper

--- app/helpers/songlists_helper.rb
+++ app/helpers/songlists_helper.rb
@@ -1,16 +1,3 @@
 module SonglistsHelper
-  def songlist_info(songlist)
 
-
-    info = {}
-
-    time        = songlist.time.strftime("%H:%M:%S")
-    singer      = song.singer.name_info
-    composer    = song.composer.name_info
-    title       = song.title_info
-    description = song.description
-
-    if singer.empty? and composer.empty? and title.empty?
-    end
-  end
 end
