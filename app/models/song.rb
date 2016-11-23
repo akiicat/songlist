c2209eedd2912589a5785cc2161ec1a42527569f
aaaa1379 <aaaa1379@gmail.com>
2016-11-23 18:49:49 +0800

    all song info

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,4 +12,19 @@ class Song < ApplicationRecord
       "#{self.title} / #{self.title_translation}"
     end
   end
+
+  def song_info
+    singer     = self.singer.name_info if self.singer
+    composer   = self.composer.name_info if self.composer
+    song_info  = "#{singer} - #{self.title_info}".strip
+    supplement = "#{composer} #{self.description}".strip
+
+    if self.composer.blank? and self.description.blank? and singer.blank? and self.title_info.blank?
+      ""
+    elsif self.composer.blank? and self.description.blank?
+      "#{song_info}"
+    else
+      "#{song_info} (#{supplement})"
+    end
+  end
 end
