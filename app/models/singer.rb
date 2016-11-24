7b1779c6d5989e25becb02e6d294f366c3732a2f
Akiicat <aaaa1379@gmail.com>
2016-11-25 01:09:59 +0800

    Rename method

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,7 +1,7 @@
 class Singer < ApplicationRecord
   has_many :songs
 
-  def name_info
+  def name_with_translation
     if self.name_translation.blank?
       "#{self.name}"
     else
--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ApplicationRecord
   belongs_to :singer  , :class_name => "Singer", :foreign_key => "singer_id"
   belongs_to :composer, :class_name => "Singer", :foreign_key => "composer_id"
 
-  def title_info
+  def title_with_translation
     if self.title_translation.blank?
       "#{self.title}"
     else
@@ -14,9 +14,9 @@ class Song < ApplicationRecord
   end
 
   def name_all
-    singer     = self.singer.name_info   if self.singer
-    composer   = self.composer.name_info if self.composer
-    title      = self.title_info
+    singer     = self.singer.name_with_translation   if self.singer
+    composer   = self.composer.name_with_translation if self.composer
+    title      = self.title_with_translation
     supplement = "#{composer} #{self.description}".strip
 
     results = (singer.blank? or title.blank?) ? "#{singer} #{title}" : "#{singer} - #{title}"
