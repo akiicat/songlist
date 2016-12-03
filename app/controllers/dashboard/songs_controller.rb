cb73376db54811615030105bcc348113e0e176aa
Akiicat <aaaa1379@gmail.com>
2016-12-04 05:54:56 +0800

    Add export

--- app/controllers/dashboard/songs_controller.rb
+++ app/controllers/dashboard/songs_controller.rb
@@ -9,6 +9,7 @@ class Dashboard::SongsController < Dashboard::ApplicationController
     respond_to do |format|
       format.html
       format.js   { render :file => "dashboard/songs/ajax/search.js.erb" }
+      format.json { render :json => @songs.map{ |s| export_hash(s) }.to_json }
     end
   end
 
@@ -82,5 +83,31 @@ class Dashboard::SongsController < Dashboard::ApplicationController
       args['composer_id'] = Singer.find_singer(composer['name'], composer['name_translation']).id
 
       args
+    end
+
+    # export
+    def export_hash song
+      {
+        singer: {
+          name: song.singer.name.to_s,
+          name_translation: song.singer.name_translation.to_s,
+        },
+
+        composer: {
+          name: song.composer.name.to_s,
+          name_translation: song.composer.name_translation.to_s,
+        },
+
+        title: song.title.to_s,
+        title_translation: song.title_translation.to_s,
+
+        cover: song.cover.to_s,
+        feat: song.feat.to_s,
+
+        description: song.description.to_s,
+
+        lyric_url: song.lyric_url.to_s,
+        video_url: song.video_url.to_s,
+      }
     end
 end
