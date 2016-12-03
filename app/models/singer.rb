4fcfb6bd81a9e19c6c8028f710e99498439af39f
Akiicat <aaaa1379@gmail.com>
2016-12-04 05:58:50 +0800

    init counter cache

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -9,6 +9,8 @@ class Singer < ApplicationRecord
 
   def self.find_singer(name, trans = nil)
     trans ||= ""
-    Singer.find_by(name: name, name_translation: trans) || Singer.create(name: name, name_translation: trans)
+    rtn   = Singer.find_by(name: name, name_translation: trans)
+    rtn ||= Singer.create(name: name, name_translation: trans, count_of_singer_songs: 0, count_of_composer_songs: 0)
+    rtn
   end
 end
