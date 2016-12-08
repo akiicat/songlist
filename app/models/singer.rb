274bb406341b89805a09c4c0fef41b59c8d61e07
Akiicat <aaaa1379@gmail.com>
2016-12-08 12:25:50 +0800

    Add singer check

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,7 +1,9 @@
 class Singer < ApplicationRecord
   has_many :songs
+  has_many :composers_songs, foreign_key: :composer_id, class_name: 'Song'
 
   default_scope { order(count_of_singer_songs: :desc, count_of_composer_songs: :desc) }
+  after_update :check_singer
 
   def name_with_translation
     rtn  = "#{self.name}"
@@ -10,7 +12,21 @@ class Singer < ApplicationRecord
   end
 
   def check_singer
-    
+    repeat_singers = Singer.unscoped.select(:name,:name_translation).group(:name,:name_translation).having("count(*) > 1")
+    repeat_singers.each do |singer|
+      singers = Singer.where(name: singer.name, name_translation: singer.name_translation).to_a
+      target_id = singers.shift.id
+
+      singers.each do |s|
+        Song.unscoped.where(singer_id: s.id).update_all(singer_id: target_id)
+        Song.unscoped.where(composer_id: s.id).update_all(composer_id: target_id)
+        Singer.reset_counters(s.id, :songs, :composers_songs)
+      end
+
+      Singer.reset_counters(target_id, :songs, :composers_songs)
+    end
+
+    Singer.where(count_of_singer_songs: 0, count_of_composer_songs: 0).delete_all
   end
 
   def self.find_singer(name, trans = nil)
