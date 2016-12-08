ae1e6537b61165bbcf4fe1ace972524ed4c6941a
Akiicat <aaaa1379@gmail.com>
2016-12-08 09:10:10 +0800

    Move export hash to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+private
+
+  def export_hash song
+    {
+      singer: {
+        name: song.singer.name.to_s,
+        name_translation: song.singer.name_translation.to_s,
+      },
+
+      composer: {
+        name: song.composer.name.to_s,
+        name_translation: song.composer.name_translation.to_s,
+      },
+
+      title: song.title.to_s,
+      title_translation: song.title_translation.to_s,
+
+      cover: song.cover.to_s,
+      feat: song.feat.to_s,
+
+      description: song.description.to_s,
+
+      lyric_url: song.lyric_url.to_s,
+      video_url: song.video_url.to_s,
+    }
+  end
 end
