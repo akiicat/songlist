d26b8a3e6f3138ab78fe44f1649ae62e3ea94a56
Akiicat <aaaa1379@gmail.com>
2016-12-08 09:13:37 +0800

    Clippy

---
+++ app/assets/javascripts/clippy.js
@@ -0,0 +1,28 @@
+function clippy() {
+  $('[data-clippy]').click(function(e) {
+    var song_name = $(this).data('clippy');
+    copyTextToClipboard(song_name);
+  });
+}
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+}
+
+$(document).ready(clippy);
