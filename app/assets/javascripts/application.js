a548c61355a55e9d1c318646bcb4f4ba15325b38
aaaa1379 <aaaa1379@gmail.com>
2016-12-13 16:59:03 +0800

    update delete confirm

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,40 @@ function searching() {
 }
 
 $(document).on('ready turbolinks:load', searching);
+
+//Override the default confirm dialog by rails
+$.rails.allowAction = function(link){
+  if (link.data("confirm") == undefined){
+    return true;
+  }
+  $.rails.showConfirmationDialog(link);
+  return false;
+}
+//User click confirm button
+$.rails.confirmed = function(link){
+  link.data("confirm", null);
+  link.trigger("click.rails");
+}
+//Display the confirmation dialog
+$.rails.showConfirmationDialog = function(link){
+  var message = link.data("confirm");
+  swal({
+    title: "你確定嗎？",
+    text: message,
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "沒錯！就這樣辦！",
+    cancelButtonText: "取消",
+    closeOnConfirm: false,
+    closeOnCancel: true,
+    showLoaderOnConfirm: true,
+    allowOutsideClick: true
+  },
+  function(isConfirm){
+    if (isConfirm) {
+      $.rails.confirmed(link);
+    } else {
+    }
+  });
+}
