b45881b4fc22fc412bc1aef0e3a1517655f43462
Akiicat <aaaa1379@gmail.com>
2016-12-14 13:37:37 +0800

    Update router remove old content

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   root to: "home/songs#index"
   scope module: :home do
-    resources :playlists
-    resources :songs
+    resources :songs, only: [:index]
   end
   namespace :dashboard do
-    resources :playlists
-    resources :songlists
-    resources :songinfos
-    resources :songs
-    resources :singers
+    root to: "songs#index"
+    resources :songs, except: [:show]
+    resources :singers, except: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
