2f5949f7073e991498c2061889e7dd9c221eba38
Akiicat <aaaa1379@gmail.com>
2016-12-19 18:15:22 +0800

    Open singer pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :dashboard do
     root to: "songs#index"
     resources :songs, except: [:show]
-    resources :singers, except: [:index, :show]
+    resources :singers
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
