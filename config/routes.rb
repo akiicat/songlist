c9fe75992906ef887405d9780f891b44173e21c5
Akiicat <aaaa1379@gmail.com>
2017-01-30 13:33:52 +0800

    Show song video

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
   namespace :dashboard do
     root to: "songs#index"
-    resources :songs, except: [:show]
+    resources :songs
     resources :singers
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
