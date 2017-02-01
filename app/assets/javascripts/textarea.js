84a536b67c6d126a8322b97ff521f394eaf7e910
Akiicat <aaaa1379@gmail.com>
2017-02-01 22:58:15 +0800

    Update textarea add range

--- app/assets/javascripts/textarea.js
+++ app/assets/javascripts/textarea.js
@@ -1,9 +1,11 @@
 function fit_textarea(){
-  $('.content').on( 'change keyup keydown paste cut', 'textarea', function (){
+  $('.content').on('change keyup keydown paste cut', 'textarea', function (){
     var scrollBottom = window.pageYOffset;
-    $(this).height(0).height(this.scrollHeight);
+    if ($(this).height() < this.scrollHeight - 8) {
+      $(this).height(0).height(this.scrollHeight + 28);
+    }
     window.scrollTo(0, scrollBottom);
-  }).find( 'textarea' ).change();
+  }).find('textarea').change();
 }
 
 $(document).ready(fit_textarea);
