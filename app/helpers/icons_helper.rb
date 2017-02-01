69904e1cfcca2efe764b529d45b32a079e7db8f4
Akiicat <aaaa1379@gmail.com>
2017-02-01 23:56:58 +0800

    Update icon tag refer to link_to api

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,18 +1,20 @@
 module IconsHelper
-  def hint_tag(aria = nil, hint = nil, options = {}, &block)
+  def hint_tag(name = nil, aria = nil, hint = nil, options = nil, &block)
+    options, hint, aria, name = hint, aria, name, block if block_given?
+    options ||= {}
+
     options["data-aria-label"] = aria if aria
     options["data-tipsy-hint"] = hint if hint
-    content_tag :span, options do
-      block.call
-    end
+
+    content_tag(:span, name, options, &block)
   end
-  def copy_tag(data = nil, options = {}, &block)
-    if data
-      options["data-clippy"] = data
-      options["data-clippy-duplicate"] = false
-    end
-    content_tag :span, options do
-      block.call
-    end
+
+  def copy_tag(name = nil, data = nil, options = nil, &block)
+    options, data, name = data, name, block if block_given?
+    options ||= {}
+
+    options["data-clippy"], options["data-clippy-duplicate"] = data, false if data
+
+    content_tag(:span, name, options, &block)
   end
 end
