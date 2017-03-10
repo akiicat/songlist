400076011a7d5572e7c086453242acd7a99a3ca7
aaaa1379 <aaaa1379@gmail.com>
2017-03-10 10:42:35 +0800

    update to new version 5.0.2

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 gem 'will_paginate'
 gem 'octicons_helper'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails', '~> 5.0.2', '>= 5.0.2'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 gem 'pg'
--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -2,30 +2,22 @@
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
 #
-# Read the Rails 5.0 release notes for more info on each option.
+# Once upgraded flip defaults one by one to migrate to the new default.
+#
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = true
+Rails.application.config.action_controller.per_form_csrf_tokens = false
 
 # Enable origin-checking CSRF mitigation. Previous versions had false.
-Rails.application.config.action_controller.forgery_protection_origin_check = true
+Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = true
+ActiveSupport.to_time_preserves_timezone = false
 
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = false
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
-ActiveSupport.halt_callback_chains_on_return_false = false
-
-# Configure SSL options to enable HSTS with subdomains. Previous versions had false.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
-
-# DEPRECATION WARNING: Time columns will become time zone aware in Rails 5.1.
-# This still causes `String`s to be parsed as if they were in `Time.zone`, and `Time`s to be converted to `Time.zone`.
-# To silence this deprecation warning, add the following:
-# config.active_record.time_zone_aware_types = [:datetime, :time]
-# To keep the old behavior, you must add the following to your initializer:
-Rails.application.config.active_record.time_zone_aware_types = [:datetime]
+ActiveSupport.halt_callback_chains_on_return_false = true
